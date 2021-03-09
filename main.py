c587b75f316156538c196afbcab311b9bda79fe1
quinnshim2021 <quinnshim2021@u.northwestern.edu>
2021-03-08 20:11:28 -0600

    viz

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from clean import *
 from eda import eda
 from modeling import *
 import pandas
+import plotly
+import plotly.graph_objects as go
+import plotly.express as px
+from urllib.request import urlopen
+import json
 
 def runModeling(cleaned):
     # pairing to test
@@ -19,12 +24,33 @@ def runModeling(cleaned):
     ]
 
     linearRegression(cleaned, "cases_pc", "percent_Trump")
+    f = open("modelingResults.txt", "a")  # append mode 
     for pair in pairings:
-        pearson(cleaned, pair[1], pair[0])
-        spearman(cleaned, pair[1], pair[0])
-        linearRegression(cleaned, pair[1], pair[0])
+        f.write(pearson(cleaned, pair[1], pair[0]))
+        f.write(spearman(cleaned, pair[1], pair[0]))
+        f.write(linearRegression(cleaned, pair[1], pair[0]))
+        f.write('\n\n')
+    f.close()
     
     modeling(cleaned)
+
+def draw(cleaned, category):
+    cleaned['infrequent_mask_use'].astype(float)
+    cleaned['fips'].astype(str)
+
+    # from https://plotly.com/python/mapbox-county-choropleth/
+    with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+        counties = json.load(response)
+    fig =  px.choropleth_mapbox(cleaned, geojson=counties, locations='fips', color=category, # trump or infrequent mask use
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 1), # percentage, so only need 0 through 1
+                           mapbox_style="carto-positron",
+                           zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
+                           opacity=0.5,
+                           labels={}
+                          )
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.show()
 
 
 # first grabs data, then cleans, then does eda
@@ -42,8 +68,12 @@ def main():
     # modeling
     runModeling(cleaned)
 
-    # run eda
-    #eda(cleaned)
+    # visualization
+    draw(cleaned, "infrequent_mask_use")
+    draw(cleaned, "percent_Trump")
+
+    # # run eda
+    eda(cleaned)
 
 if __name__ == '__main__':
     main()
--- modeling.py
+++ modeling.py
@@ -12,10 +12,10 @@ import matplotlib.pyplot as plt
 
 
 def pearson(data, category_a, category_b):
-    print(f'Pearson Coefficient ({category_a}, {category_b}):\t', pearsonr(data[category_a], data[category_b]))
+    return f'Pearson Coefficient ({category_a}, {category_b}):\t{pearsonr(data[category_a], data[category_b])}'
 
 def spearman(data, category_a, category_b):
-    print(f'Spearman Coefficient ({category_a}, {category_b}):\t', spearmanr(data[category_a], data[category_b]))
+    return f'Spearman Coefficient ({category_a}, {category_b}):\t{spearmanr(data[category_a], data[category_b])}'
 
 def linearRegression(data, category_a, category_b):
     X_train, X_test, Y_train, Y_test = train_test_split(data[category_a].to_numpy().reshape(-1, 1), data[category_b].to_numpy(), test_size=0.2)
@@ -23,7 +23,7 @@ def linearRegression(data, category_a, category_b):
     linear_model = LinearRegression()
     model = linear_model.fit(X_train, Y_train)
     r2_linear = model.score(X_test, Y_test)
-    print(f'R2 of Linear Regression ({category_a}, {category_b}): {r2_linear}')
+    return f'R2 of Linear Regression ({category_a}, {category_b}): {r2_linear}'
 
 def modeling(data):
     #print(chiSquareDeaths(data))
